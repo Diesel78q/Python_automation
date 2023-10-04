5dd840882b2b640ae80f3e2e49b7986fbe3cccb6
Diesel78q <daniilgoncar7@gmail.com>
2023-10-05 00:48:08 +0200

    f

--- pytest_lessons/tests/test_something.py
+++ pytest_lessons/tests/test_something.py
@@ -5,6 +5,7 @@ from src.enums.global_enums import GlobalErrorMessages
 def test_getting_POSTs():
     response = requests.get(url=SERVICE_URL)
     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    
+    received_posts = response.json()
+    assert len(received_posts) == 7, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
     
     print(response.json())
